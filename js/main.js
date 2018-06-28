4bb56e02f690d2eea0234d4c66d9588599ad110b
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2018-06-28 02:58:42 -0500

    js and media added

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+*/
+
+$(document).ready(
+	function(){
+		$(".menu").click(function(){
+			$(".menu").toggleClass("menu-after");
+			$("#mainNav").toggleClass("overlay-after");
+			$(".job-name").toggleClass("job-name-after");
+
+
+
+		});
+	})
